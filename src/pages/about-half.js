b50e413eae0860981c8abcb039e4c93f4a71d97a
Taniya Yadav <128471154+Taniya23Y@users.noreply.github.com>
2024-06-09 14:29:25 +0530

    changes some file name and content

---
+++ src/pages/about-half.js
@@ -0,0 +1,10 @@
+const PrachiTunePura = () => {
+  return (
+    <div className="w-[78.5rem] relative text-[3.125rem] font-medium font-inter text-white text-center inline-block h-[25.5rem]">
+      <p className="m-0 whitespace-pre-wrap">{`Remaining Part  `}</p>
+      <p className="m-0">{`Display Here `}</p>
+    </div>
+  );
+};
+
+export default PrachiTunePura;
